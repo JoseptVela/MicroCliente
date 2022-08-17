e0c6e21c9618b578dabfe136d71836dc41955469
Josept <hvelab3e@gmail.com>
2022-08-16 22:11:41 -0500

    MicroCliente

---
+++ EF_HarlyVela_MicroCliente/src/main/java/com/idat/MicroCliente/service/ClienteService.java
@@ -0,0 +1,15 @@
+package com.idat.MicroCliente.service;
+
+import java.util.List;
+
+import com.idat.MicroCliente.model.Cliente;
+
+public interface ClienteService {
+	
+	List<Cliente> listar();
+	Cliente obtenerId(Integer id);
+	void guardar(Cliente cliente);
+	void eliminar(Integer id);
+	void actualizar(Cliente cliente);
+
+}
---
+++ EF_HarlyVela_MicroCliente/src/test/java/com/idat/MicroCliente/EfHarlyVelaMicroClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idat.MicroCliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfHarlyVelaMicroClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
